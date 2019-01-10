40b12959c95ea9809d7dc9bf55a72cf9d9cf857b
Leannelb <leannelaceybyrne@outlook.com>
2019-01-10 20:43:30 +0100

    app-root must be present at end of index.html file

---
+++ src/app/components/nav/nav.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'navbar',
+  templateUrl: 'nav.component.html',
+  styleUrls: ['nav.component.css']
+})
+export class NavComponent {
+  title = 'nav';
+}
